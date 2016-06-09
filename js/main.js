8486bd957ec90a34e60a2fe9825c3642e4da296c
hljuan <2105474372@qq.com>
2016-06-10 03:43:06 +0800

    我都不知道在干什么2.7

--- js/main.js
+++ js/main.js
@@ -2,9 +2,39 @@ var vm = new Vue({
     el: '#app',
     data: {
         banner: '小柔柔的主页',
-        backgroundColor: '#A8B3DB'
+        backgroundColor: '#fff',
+        begin : new Date('2012/09/13 00:00:00').getTime(),
+        yy: '',
+        MM: '',
+        dd: '',
+        hh: '',
+        mm: '',
+        ss:''
     }
 });
+
+
+var toDoubleDigit = function (number) {
+    number = number.toString();
+    return number.length == 1 ? "0" + number : number;
+};
+
+var tik = function () {
+    var today = new Date().getTime();
+    var period = today - vm.begin;
+
+    var time = new Date(period);
+
+    vm.yy = toDoubleDigit(time.getFullYear()-1970);
+    vm.MM = toDoubleDigit(time.getMonth() + 1);
+    vm.dd = toDoubleDigit(time.getDate());
+    vm.hh = toDoubleDigit(time.getHours());
+    vm.mm = toDoubleDigit(time.getMinutes());
+    vm.ss = toDoubleDigit(time.getSeconds());
+    setTimeout('tik()', 1000);
+};
+
+tik();
 
 var App = Vue.extend({
 
@@ -12,12 +42,14 @@ var App = Vue.extend({
 
 var router = new VueRouter();
 
+
 router.map({
     '/cute': {
         component: {
             template: '#cute',
             attached: function () {
-                vm.banner = '萌妹子';
+                $('#time').css('display', 'none');
+                vm.banner = '萌妹纸';
                 vm.backgroundColor = 'rgb(72, 71, 71)';
             }
         }
@@ -27,9 +59,14 @@ router.map({
             template: '#godness'
         }
     },
-    '/graduation': {
+    '/time': {
         component: {
-            template: '#graduation'
+            template: '',
+            attached: function () {
+                vm.banner = '流年';
+                vm.backgroundColor = '#A8B3DB';
+                $('#time').css('display', 'block');
+            }
         }
     }
 });
